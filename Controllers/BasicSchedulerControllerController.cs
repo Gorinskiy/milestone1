86af5610f9e17e46ed9aec48148f50d372af6c9f
Gorinskiy <60805410+Gorinskiy@users.noreply.github.com>
2020-12-02 15:24:33 +0600

    Initial commit

---
+++ Controllers/BasicSchedulerControllerController.cs
@@ -0,0 +1,24 @@
+﻿using DHTMLX.Common;
+using DHTMLX.Scheduler;
+using DHTMLX.Scheduler.Data;
+using Milestone1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Milestone1.Controllers
+{
+    public class BasicSchedulerController : Controller
+    {
+        public ActionResult Index()
+        {
+            var sched = new DHXScheduler(this);
+            sched.Skin = DHXScheduler.Skins.Terrace;
+            sched.LoadData = true;
+            sched.EnableDataprocessor = true;
+            sched.InitialDate = new DateTime(2016, 5, 5);
+            return View(sched);
+        }
+    }
+}
---
+++ Models/SchedulerContext .cs
@@ -0,0 +1,19 @@
+﻿using DHTMLX.Scheduler;
+using Microsoft.EntityFrameworkCore;
+using Milestone1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Milestone1.Controllers
+{
+    public partial class SchedulerContext : DbContext
+    {
+        private const string V = "name=SchedulerContext";
+
+        
+        public virtual DbSet<Event> Events { get; set; }
+    }
+}
