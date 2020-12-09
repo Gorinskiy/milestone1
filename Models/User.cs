790e0299352cbecb2ede05d7a43bddb76efcf24f
Gorinskiy <60805410+Gorinskiy@users.noreply.github.com>
2020-12-09 17:07:15 +0600

    v

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace CustomIdentityApp.Models
+{
+    public class User : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+}
